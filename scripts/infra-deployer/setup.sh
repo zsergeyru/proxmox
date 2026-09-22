24ddb5a5695c2e19551b36d8816429634a54926f
isergey <52747380+zsergeyru@users.noreply.github.com>
2026-09-22 13:31:32 +0300

    fix: разрешить Semaphore запись в каталог SQLite

--- scripts/infra-deployer/setup.sh
+++ scripts/infra-deployer/setup.sh
@@ -56,7 +56,8 @@ check_os() {
 prepare_directories() {
     install -d -o root -g root -m 0755 "$CONFIG_DIR" "$CA_DIR" "$DATA_DIR" "$COMPOSE_DIR"
     install -d -o root -g root -m 0700 "$SECRET_DIR"
-    install -d -o 1001 -g 0 -m 0750 "$SEMAPHORE_DIR" "$RUNNER_DIR" "$RUNNER_TMP_DIR" "$OPENTOFU_DIR" "$STATE_DIR"
+    install -d -o 1001 -g 0 -m 0770 "$SEMAPHORE_DIR"
+    install -d -o 1001 -g 0 -m 0750 "$RUNNER_DIR" "$RUNNER_TMP_DIR" "$OPENTOFU_DIR" "$STATE_DIR"
     install -d -o root -g root -m 0755 "$PUBLIC_KEY_DIR"
 
     # Semaphore Server в официальном образе работает от UID 1001.
